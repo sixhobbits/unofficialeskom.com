0436d5c5bf40f0c7f870458c7925218cedc0a808
Gareth Dwyer <gareth@ritza.co>
2023-01-28 22:37:44 +0200

    update

--- build.sh
+++ build.sh
@@ -4,7 +4,12 @@ source venv/bin/activate
 mkdocs build
 
 git add .
-git commit -m "{$1}"
+if [ "$1" != "" ] # or better, if [ -n "$1" ]
+then
+    git commit -m "$1"
+else
+    git commit -m update
+fi
 git push origin main
 
 rsync -hvrPt site/* het:/var/www/unofficialeskom.com/
